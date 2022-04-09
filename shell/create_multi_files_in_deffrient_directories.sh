1ce0b61cbb8b2cdc5cd8e47a99cb0e1d97d2bb41
Mahmoud-Da <mdabbagh6@gmail.com>
2022-04-09 23:24:21 +0900

    フォルダツリーを作成する

--- shell/create_multi_files_in_deffrient_directories.sh
+++ shell/create_multi_files_in_deffrient_directories.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 ROOT='out'
 mkdir "$ROOT"
 
-
-for ((i=1;i<='$#';i++)); do
-    mkdir "$ROOT/$i"
-    for ((j=1;j<='$i';j++)); do
-        touch "$ROOT/$i/$j"
+for ((i=1;i<=$#;i++)); do
+    mkdir "$ROOT/dir_$i"
+    for ((j=1;j<= ${!i} ;j++)); do
+      touch "$ROOT/dir_$i/file_$j.txt"
     done
 done
