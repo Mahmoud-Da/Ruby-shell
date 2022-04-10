6911647449a2d684545beff3fa1935ccfba75e23
Mahmoud-Da <mdabbagh6@gmail.com>
2022-04-10 14:11:16 +0900

    全体修正

--- shell/create_multi_files_in_deffrient_directories.sh
+++ shell/create_multi_files_in_deffrient_directories.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-ROOT='out'
-mkdir "$ROOT"
-
-for ((i=1;i<=$#;i++)); do
-    mkdir "$ROOT/dir_$i"
-    for ((j=1;j<= ${!i} ;j++)); do
-      touch "$ROOT/dir_$i/file_$j.txt"
-    done
+root='out'
+mkdir "$root"
+for (( i=1;i<=$#;i++ )); do
+  mkdir "$root/dir_$i"
+  for (( j=1;j<= ${!i} ;j++ )); do
+    touch "$root/dir_$i/file_$j.txt"
+  done
 done
