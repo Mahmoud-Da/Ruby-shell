5b2ca03b7131ec9dd472eb53f7d93b4cfeb1996a
Mahmoud-Da <mdabbagh6@gmail.com>
2022-04-12 13:02:52 +0900

    修正途中

--- ruby/operation_ex.rb
+++ ruby/operation_ex.rb
@@ -1,7 +1,7 @@
-num = "25"
-p num.to_i
-p num.to_f
-p num.to_r
-p num.to_c
+number = "25"
+p number.to_i
+p number.to_f
+p number.to_r
+p number.to_c
 
 puts 25 + (25.0).to_i + (25/1).to_i + (25+0i).to_i
