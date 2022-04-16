67bbdc76a7fba8321c902c38be772bed6b074b4d
Mahmoud-Da <mdabbagh6@gmail.com>
2022-04-16 15:50:36 +0900

    全体修正と改善

--- shell/create_log_file.sh
+++ shell/create_log_file.sh
@@ -1,8 +1,47 @@
 #!/bin/bash
-cd log
-$1
-if [ "$1" == "" ]; then
-  echo "ログ名をお追加お願いします！"
+function usage {
+  cat <<EOF
+
+使用方法:
+  $(basename ${0}) [-h|--help] ダイレクトリー指定します
+
+注意：
+ファイルはすでに作成されている場合は二度を作成できません。
+
+例：
+./create_log_file.sh ruby
+
+オプション:
+  --help, -h    ヘルプを表示
+
+EOF
+
+exit 2
+}
+
+
+if [ $# -eq 0 ]; then
+  printf "\033[31m%s\033[m\n" "ERROR: ログファイルを記入してください"
+  usage
+  exit 1
+fi
+
+log_place=$1
+
+cd "$(dirname $0)"/..
+
+if [ $1 = "-h" -o $1 = "--help" ]; then
+  usage
+elif [ $1 == 'ruby' ]; then
+  cd ruby/log/
+  touch "$(date +%F).txt"
+  echo "ログはrubyファイルに作成されました。"
+elif [ $1 == 'shell' ]; then
+  cd shell/log/
+  touch "$(date +%F).txt"
+  echo "ログはshellファイルに作成されました。"
 else
-  cat log_sample.txt >> $1.txt
+  printf "\033[31m%s\033[m\n" "ERROR: 下記の使用方法を再度ご確認ください"
+  usage
+  exit 1
 fi
