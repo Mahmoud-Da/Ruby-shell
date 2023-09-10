a2b97ed2a88f15107e43cdaa9a3c75e255fdfe3a
Mahmoud-Da <mdabbagh6@gmail.com>
2023-09-10 16:21:02 +0900

    finish chapter[number]

--- ruby/Ruby技術者認定試験合格教本（まとめ）/第二章/20230910.rb
+++ ruby/Ruby技術者認定試験合格教本（まとめ）/第二章/20230910.rb
@@ -1,4 +1,4 @@
-# RVMとは：
+"RVMとは"
 Mac OS用の複数Rubyバージョンを切り替えることができるオープンソースのツールのことです。
 
 def add(a, b)
@@ -21,7 +21,7 @@ end
 hoge = Hoge.new
 hoge.test
 
-# require vs load
+"require vs load"
 require #=> なんでも実行しても一回しか実行しない　/ 自動的に補完する
 load #=> 実行された回数だけ実行する　/ 補完しない
 
