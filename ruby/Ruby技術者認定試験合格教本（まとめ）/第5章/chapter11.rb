f1e01dd7b4ed42496eeb622baeae448f720e78a2
Mahmoud-Da <mdabbagh6@gmail.com>
2023-10-19 20:12:21 +0900

    IO class 1

--- ruby/Ruby技術者認定試験合格教本（まとめ）/第5章/chapter11.rb
+++ ruby/Ruby技術者認定試験合格教本（まとめ）/第5章/chapter11.rb
@@ -1,1 +1,68 @@
 "IO"
+# Filrクラスのスーパークラスでもあり、基本的な機能を揃えたクラスです。多くのメソッドはFileクラスでも利用可能
+"ファイル開く"
+# karnelのopenメソッドを利用する
+io = open("b.tsx")
+#自動的に閉じる
+open("b.tsx") { |file| file.read } #=> "let b: string;"
+
+"バイブを開く"
+*popen # コマンドをサブプロセスとして実行し、そのプロセスと入出力のバイブを開くことができます。
+
+IO.popen("grep -i ruby", "r+") do |io|
+  io.write("this is Ruby program")
+  io.close_write
+  puts io.read
+end
+#=> this is Ruby program
+
+"IOからの入力"
+*read # 内容を読み込み。長さも指定可能 クラスメソッド・インスタンスメソッド
+*each / each_lines / for_each # 各ブロックに渡して実行可能
+*readline #  readlineクラスメソッド{全てのファイるを読み取り、配列として戻す}
+*readline / gets # get・readlineインスタンスメソッド(一行読み込む) readlineの場合は最後の行に到着したら「EOFError」が発生する
+*each_byte # バイト取得 クラスメソッド
+*getbyte / readbyte # バイト取得
+*each_char # 文字一つずつを読むこむ クラスメソッド
+*getc / readchar # 文字に対応する文字を返す
+
+
+IO.read("b.tsx") #=> "let b: string;"
+IO.read("b.tsx", 3) #=> "let"
+
+
+IO.foreach("b.tsx") {|line| puts line }   #=> let b: string;
+
+io = open("b.tsx") #=> #<File:b.tsx>
+io.gets #=> "let b: string;"
+io.readline # (irb):37:in `readline': end of file reached (EOFError)
+io.gets #=> nil 
+io.close
+
+io = open("b.tsx") #=> #<File:b.tsx>
+io.getc #=> "l"
+io.getc #=> "e"
+io.close
+
+"空ファイルの場合"
+IO.read # "" を返す {指定があればnilを返す}
+IO.readline # 空配列 ([])
+IO.for_each # プロック実行されない
+
+each / for_each # EOFであれば何もしない
+getc / gets # nil
+read # "" を返す {指定があればnilを返す}
+readchar / readline # EOFError
+readlines # 空配列 ([])
+getbyte # nil
+readbyte # EOFError
+
+
+"IOからの出力"
+*write # IOに対して、引数の文字列を出力し、引数が文字例以外の場合は、_to.sメソッドで文字列化して出力する。
+*puts # IOに対して、複数のオプジェクトを出力し、引数が文字例や配列以外の場合は、まず_to.aryメソッドで配列化、その後_to.sメソッドで文字列化して出力する。
+*print # 
+*printf # C言語と同じで指定されたフォーマットで返す
+*putc # 
+<< # 
+*flush # 
