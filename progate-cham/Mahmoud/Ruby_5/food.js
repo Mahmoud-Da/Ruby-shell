159c74e8dbcd58602855f5fa74b711748e917bf4
Mahmoud-Da <mdabbagh6@gmail.com>
2024-09-22 14:25:24 +0900

    add JS code similar to ruby

--- progate-cham/Mahmoud/Ruby_5/food.js
+++ progate-cham/Mahmoud/Ruby_5/food.js
@@ -1,3 +1,16 @@
 import { Menu } from "./menu";
 
-class Food extends Menu {}
+class Food extends Menu {
+  constructor(name, price, calorie) {
+    super(name, price);
+    this.calorie = calorie;
+  }
+
+  info() {
+    return `${this.name} ${this.price}円 (${this.calorie}kcal)`;
+  }
+
+  calorie_info() {
+    return `${this.name}は(${this.calorie}kcal)`;
+  }
+}
